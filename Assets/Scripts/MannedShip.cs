6effb144f6d0e49aff36ef085f1c7e2d14369cb2
antonioferreira312 <antonioferreira.git@gmail.com>
2021-02-13 23:13:48 -0300

    Initial commit

---
+++ Assets/Scripts/MannedShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannedShip : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(Input.acceleration.x*-100, 0, Input.acceleration.y * -100);
+        Debug.Log(Input.accelerationEvents.Clone());
+    }
+}
